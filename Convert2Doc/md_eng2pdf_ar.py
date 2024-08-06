78b5475570012d5d5a33888591f47f29a209a8e1
Peter Jones <4502762+peterwjones334@users.noreply.github.com>
2024-08-06 21:00:04 +0100

    Update md_eng2pdf_ar.py
    fixup fonts

--- Convert2Doc/md_eng2pdf_ar.py
+++ Convert2Doc/md_eng2pdf_ar.py
@@ -1,4 +1,7 @@
 # pip install fpdf arabic-reshaper python-bidi googletrans==4.0.0-rc1
+# https://github.com/aliftype/amiri/releases/tag/1.000
+# https://www.1001fonts.com/download/scheherazade.zip
+# https://fonts.google.com/share?selection.family=Amiri:ital,wght@0,400;0,700;1,400;1,700|Lateef:wght@200;300;400;500;600;700;800
 
 from fpdf import FPDF
 import arabic_reshaper
@@ -38,7 +41,7 @@ def create_pdf(input_markdown_file, output_pdf_file, font_path):
 
     # Translate text to Arabic
     translator = Translator()
-    translation = translator.translate(english_text, src='en', dest='ar')
+    translation = translator.translate(english_text, src='en', dest='ar') #
     arabic_text = translation.text
 
     # Split the Arabic text into lines for the PDF
@@ -51,9 +54,9 @@ def create_pdf(input_markdown_file, output_pdf_file, font_path):
     pdf.output(output_pdf_file)
 
 # Usage example
-input_markdown_file = 'D:\\Process\\Project77\\test.md'  # Your English markdown file
-output_pdf_file = 'D:\\Process\\Project77\\output_arabic2.pdf'
-font_path = 'D:\\Process\\Project77\\Amiri-Regular.ttf'  # Path to your Arabic font file
+input_markdown_file = 'input_english.md'  # Your English markdown file
+output_pdf_file = 'output_arabic.pdf'
+font_path = 'Amiri-Regular.ttf'  # Path to your Arabic font file "Amiri",  "Scheherazade" or "Lateef".
 
 create_pdf(input_markdown_file, output_pdf_file, font_path)
 
